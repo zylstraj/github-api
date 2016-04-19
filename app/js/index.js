40834b07f0d574cb13bad8cc3f8b3fc1ed523bd6
Josh Zylstra <zylstrajosh@gmail.com>
2016-04-19 11:14:20 -0700

    Its working now completely, rearranged how I brought in the res data

--- app/js/index.js
+++ app/js/index.js
@@ -7,7 +7,7 @@ const app = angular.module('GithubApp', [])
 app.controller('RepoController', ['$http', function($http) {
   const mainRoute = 'https://api.github.com/users/zylstraj/repos';
   var okay = this;
-  okay.myRepos = {};
+  okay.myRepos = [];
   okay.getRepos = function() {
       $http.get(mainRoute)
       .then(function(result) {
@@ -22,16 +22,18 @@ app.controller('RepoController', ['$http', function($http) {
 app.controller('AboutMeController', ['$http', function($http) {
   const mainRoute = 'https://api.github.com/users/zylstraj';
   var okay = this;
-  okay.myFollowers = {};
-  okay.myFollowing = {};
-  okay.myAvatar = {};
+  okay.myInfo = [];
+  // okay.myFollowers = [];
+  // okay.myFollowing = [];
+  // okay.myAvatar = [];
   okay.getMyInfo = function() {
     $http.get(mainRoute)
     .then(function(result) {
       console.log(result.data);
-      okay.myFollowers = result.data.followers_url;
-      okay.myFollowing = result.data.following_url;
-      okay.myAvatar = result.data.avatar_url;
+      okay.myInfo = result.data;
+      // okay.myFollowers = result.data.followers_url;
+      // okay.myFollowing = result.data.following_url;
+      // okay.myAvatar = result.data.avatar_url;
     }, function(error) {
       console.log(error);
     })
